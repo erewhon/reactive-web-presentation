356284e6cfecbc4e6905b836925c5a65a0a1dfde
Steven Byrnes <erewhon@flatland.org>
2014-11-16 18:49:15 -0600

    more tweaking to D3 base example

--- act1-html/buttonz.js
+++ act1-html/buttonz.js
@@ -1,60 +1,259 @@
+// Stackoverflow FTW!  http://stackoverflow.com/questions/20318822/how-to-create-a-stopwatch-using-javascript
+
+var Stopwatch = function(elem, options) {
+
+  var timer       = createTimer(),
+      startButton = createButton("start", start),
+      stopButton  = createButton("stop", stop),
+      resetButton = createButton("reset", reset),
+      offset,
+      clock,
+      interval;
+
+  // default options
+  options = options || {};
+  options.delay = options.delay || 1;
+
+  // append elements
+  elem.appendChild(timer);
+  elem.appendChild(startButton);
+  elem.appendChild(stopButton);
+  elem.appendChild(resetButton);
+
+  // initialize
+  reset();
+
+  // private functions
+  function createTimer() {
+    return document.createElement("span");
+  }
+
+  function createButton(action, handler) {
+    var a = document.createElement("a");
+    a.href = "#" + action;
+    a.innerHTML = action;
+    a.addEventListener("click", function(event) {
+      handler();
+      event.preventDefault();
+    });
+    return a;
+  }
+
+  function start() {
+    if (!interval) {
+      offset   = Date.now();
+      interval = setInterval(update, options.delay);
+    }
+  }
+
+  function stop() {
+    if (interval) {
+      clearInterval(interval);
+      interval = null;
+    }
+  }
+
+  function getTime() {
+      return clock;
+  }
+
+  function reset() {
+    clock = 0;
+    render();
+  }
+
+  function update() {
+    clock += delta();
+    render();
+  }
+
+  function render() {
+    timer.innerHTML = clock/1000;
+  }
+
+  function delta() {
+    var now = Date.now(),
+        d   = now - offset;
+
+    offset = now;
+    return d;
+  }
+
+  // public API
+  this.start  = start;
+  this.stop   = stop;
+  this.reset  = reset;
+  this.getTime = getTime;
+};
+
+var sleepWatch = new Stopwatch(document.getElementById('Sleep'));
+var workWatch = new Stopwatch(document.getElementById('Work'));
+var playWatch = new Stopwatch(document.getElementById('Play'));
+
+var svg = d3.select("body")
+	.append("svg")
+	.append("g")
+
+svg.append("g")
+	.attr("class", "slices");
+svg.append("g")
+	.attr("class", "labels");
+svg.append("g")
+	.attr("class", "lines");
+
 var width = 640,
     height = 400,
-    radius = Math.min(width, height) / 2;
-
-var color = d3.scale.category20();
+	radius = Math.min(width, height) / 2;
 
 var pie = d3.layout.pie()
-    .value(function(d) { return d.apples; })
-    .sort(null);
+	.sort(null)
+	.value(function(d) {
+		return d.value;
+	});
 
 var arc = d3.svg.arc()
-    .innerRadius(radius - 100)
-    .outerRadius(radius - 20);
+	.outerRadius(radius * 0.8)
+	.innerRadius(radius * 0.4);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+var outerArc = d3.svg.arc()
+	.innerRadius(radius * 0.9)
+	.outerRadius(radius * 0.9);
 
-d3.tsv("data.tsv", type, function(error, data) {
-  var path = svg.datum(data).selectAll("path")
-      .data(pie)
-    .enter().append("path")
-      .attr("fill", function(d, i) { return color(i); })
-      .attr("d", arc)
-      .each(function(d) { this._current = d; }); // store the initial angles
+svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  d3.selectAll("input")
-      .on("change", change);
+var key = function(d){ return d.data.label; };
 
-  var timeout = setTimeout(function() {
-    d3.select("input[value=\"oranges\"]").property("checked", true).each(change);
-  }, 2000);
+var color = d3.scale.ordinal()
+	.domain(["Sleep", "Work", "Play"])
+	.range(["#98abc5", "#8a89a6", "#7b6888"]);
 
-  function change() {
-    var value = this.value;
-    clearTimeout(timeout);
-    pie.value(function(d) { return d[value]; }); // change the value function
-    path = path.data(pie); // compute the new angles
-    path.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
-  }
-});
-
-function type(d) {
-  d.apples = +d.apples;
-  d.oranges = +d.oranges;
-  return d;
+function randomData (){
+	var labels = color.domain();
+	return labels.map(function(label){
+		return { label: label, value: Math.random() }
+	});
 }
 
-// Store the displayed angles in _current.
-// Then, interpolate from _current to the new angles.
-// During the transition, _current is updated in-place by d3.interpolate.
-function arcTween(a) {
-  var i = d3.interpolate(this._current, a);
-  this._current = i(0);
-  return function(t) {
-    return arc(i(t));
-  };
+var data = [ { label: 'Sleep', value: 0.01 + sleepWatch.getTime() },
+             { label: 'Work', value: 0.01 + workWatch.getTime() },
+             { label: 'Play', value: 0.01 + playWatch.getTime() } ];
+
+change(data);
+
+setInterval(function() {
+  change(incrementData());
+}, 1000);
+
+// function incrementData(data, id) {
+//   return data.map(function(it) {
+//     return { label: it.label, value: it.value = it.value + ((it.label === id) ? 1 : 0)  }
+//   });
+// }
+
+function incrementData(data, id) {
+  return [
+     { label: 'Sleep', value: 0.01 + sleepWatch.getTime() },
+     { label: 'Work', value: 0.01 + workWatch.getTime() },
+     { label: 'Play', value: 0.01 + playWatch.getTime() }
+   ];
 }
+
+function updateData() {
+  var newData = incrementData(data, this.id);
+  change(newData);
+  data = newData;
+}
+
+d3.selectAll(".buttonz")
+    .on("click", updateData);
+
+function change(data) {
+
+	/* ------- PIE SLICES -------*/
+	var slice = svg.select(".slices").selectAll("path.slice")
+		.data(pie(data), key);
+
+	slice.enter()
+		.insert("path")
+		.style("fill", function(d) { return color(d.data.label); })
+		.attr("class", "slice");
+
+	slice
+		.transition().duration(1000)
+		.attrTween("d", function(d) {
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				return arc(interpolate(t));
+			};
+		})
+
+	slice.exit()
+		.remove();
+
+	/* ------- TEXT LABELS -------*/
+
+	var text = svg.select(".labels").selectAll("text")
+		.data(pie(data), key);
+
+	text.enter()
+		.append("text")
+		.attr("dy", ".35em")
+		.text(function(d) {
+			return d.data.label;
+		});
+
+	function midAngle(d){
+		return d.startAngle + (d.endAngle - d.startAngle)/2;
+	}
+
+	text.transition().duration(1000)
+		.attrTween("transform", function(d) {
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				var pos = outerArc.centroid(d2);
+				pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+				return "translate("+ pos +")";
+			};
+		})
+		.styleTween("text-anchor", function(d){
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				return midAngle(d2) < Math.PI ? "start":"end";
+			};
+		});
+
+	text.exit()
+		.remove();
+
+	/* ------- SLICE TO TEXT POLYLINES -------*/
+
+	var polyline = svg.select(".lines").selectAll("polyline")
+		.data(pie(data), key);
+
+	polyline.enter()
+		.append("polyline");
+
+	polyline.transition().duration(1000)
+		.attrTween("points", function(d){
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				var pos = outerArc.centroid(d2);
+				pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+				return [arc.centroid(d2), outerArc.centroid(d2), pos];
+			};
+		});
+
+	polyline.exit()
+		.remove();
+};
